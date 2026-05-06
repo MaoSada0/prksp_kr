f1420e904e05bff980ead505e8353211eafe076e
MaoSada0 <666kakryatskii666@gmail.com>
2026-05-06 09:52:14 +0300

    fix envsubst: export BACKEND_URL so child process sees it 😭🥀

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-BACKEND_URL="${BACKEND_URL:-http://backend.railway.internal:8080}"
+export BACKEND_URL="${BACKEND_URL:-http://backend.railway.internal:8080}"
 echo "[entrypoint] BACKEND_URL=$BACKEND_URL"
 envsubst '${BACKEND_URL}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
-cat /etc/nginx/conf.d/default.conf
 exec nginx -g 'daemon off;'
